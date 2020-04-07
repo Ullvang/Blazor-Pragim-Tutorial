2faa22007d7f5b8da06bf2f0d7aeac823f07901a
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-08 01:21:10 +0200

    updated project, added new

--- EmployeeManagement.Web/Program.cs
+++ EmployeeManagement.Web/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Blazor
+namespace EmployeeManagement.Web
 {
     public class Program
     {
