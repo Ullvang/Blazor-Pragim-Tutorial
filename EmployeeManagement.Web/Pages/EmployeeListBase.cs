f90bf8e03aec31c4fcae1d169411a04fa1234d95
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-10 19:22:21 +0200

    added spinner and delayed loading

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -11,14 +11,14 @@ namespace EmployeeManagement.Web.Pages
     {
         public IEnumerable<Employee> Employees { get; set; }
 
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
-            LoadEmployee();
-            return base.OnInitializedAsync();
+            await Task.Run(LoadEmployee);
         }
 
         private void LoadEmployee()
         {
+            System.Threading.Thread.Sleep(3000);
             Employee e1 = new Employee
             {
                 EmployeeId = 1,
