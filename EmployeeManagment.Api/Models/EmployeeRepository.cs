f5bc2eb2a13f2c0a1aa26284c3aef1f5e4bb6b4a
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-15 23:22:56 +0200

    display departmentname on employee

--- EmployeeManagment.Api/Models/EmployeeRepository.cs
+++ EmployeeManagment.Api/Models/EmployeeRepository.cs
@@ -37,7 +37,7 @@ namespace EmployeeManagment.Api.Models
 
         public async Task<Employee> GetEmployee(int employeeId)
         {
-            return await appDbContext.Employees.FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+            return await appDbContext.Employees.Include(e => e.Department).FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
         }
 
         public async Task<Employee> GetEmployeeByEmail(string email)
